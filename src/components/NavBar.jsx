c1897cd23d02691781d41da45176cafd2b3d28b7
stonasss <luis.b.santos96@gmail.com>
2024-06-10 09:51:44 -0300

    patched logout button positioning

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,9 +18,9 @@ export default function NavBar({ EnableAuth }) {
                 </div>
             </div> ) :(
             <div className="header z-10 top-0 fixed bg-gradient-to-r from-cyan-950 via-cyan-600 to-cyan-950 w-screen h-14 flex">
-                <div className="flex mx-auto space-x-96 items-center">
-                    <h1 className="title relative font-sans font-bold text-white text-xl hover:cursor-default">Lista de Contatos</h1>
-                        <button onClick={handleLogout} className="absolute left-64 rounded-full font-bold text-cyan-950 bg-cyan-200 w-[110px] h-[30px] text-sm hover:bg-gray-400">
+                <div className="flex relative mx-auto space-x-96 items-center">
+                    <h1 className="title font-sans font-bold text-white text-xl hover:cursor-default">Lista de Contatos</h1>
+                        <button onClick={handleLogout} className="absolute text-xs w-[60px] h-[25px] left-[-210px] sm:left-[-150px] rounded-full font-bold text-cyan-950 bg-cyan-200 sm:w-[80px] sm:h-[30px] text-sm hover:bg-gray-400">
                             Deslogar
                         </button>
                 </div>
